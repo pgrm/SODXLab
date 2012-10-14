1c5e565eff11047c192f95d39ce6a38db6f6316e
pgrm <peter.grman@gmail.com>
2012-10-14 22:57:47 +0200

    finished lock3

--- Multy/src/lock3.erl
+++ Multy/src/lock3.erl
@@ -1,6 +1,6 @@
 %% Author: peter
 %% Created: 3 Oct 2012
-%% Description: TODO: Add description to lock2
+%% Description: TODO: Add description to lock3
 -module(lock3).
 
 %%
@@ -11,73 +11,85 @@
 %%
 %% API Functions
 %%
-init(_, Nodes) ->
-    open(Nodes, 0).
+init(MyId, Nodes) ->
+    open(Nodes, MyId, 0).
 
 %%
 %% Local Functions
 %%
-open(Nodes, MyTime) ->
+open(Nodes, MyId, MyTime) ->
     receive
 	{take, Master} ->
-	    Refs = requests(Nodes, MyTime),
-	    wait(Nodes, Master, Refs, [], [], MyTime);
-	{request, From, Ref, OtherTime} ->
+	    Refs = requests(Nodes, MyId, MyTime),
+	    wait(Nodes, Master, Refs, [], MyId, MyTime, MyTime);
+	{request, From, Ref, _, OtherTime} ->
 		NewTime = new_time(MyTime, OtherTime),
 	    From ! {ok, Ref, NewTime},
-	    open(Nodes, NewTime);
+	    open(Nodes, MyId, NewTime);
 	stop ->
 	    ok
     end.
 
-requests(Nodes, MyTime) ->
+requests(Nodes, MyId, MyTime) ->
     lists:map(
       fun(P) ->
 	      R = make_ref(),
-	      P ! {request, self(), R, MyTime},
+	      P ! {request, self(), R, MyId, MyTime},
 	      {R, P}
       end,
       Nodes).
 
-wait(Nodes, Master, [], _, Waiting, MyTime) ->
+wait(Nodes, Master, [], Waiting, MyId, MyTime, _) ->
     Master ! taken,
-    held(Nodes, Waiting, MyTime);
-wait(Nodes, Master, RefsNodes, OkRefsNodes, Waiting, MyTime) ->
+    held(Nodes, Waiting, MyId, MyTime);
+wait(Nodes, Master, RefsNodes, Waiting, MyId, MyTime, RequestTime) ->
     receive
-	{request, From, Ref, OtherTime} ->
-	    {RefsNodes2, OkRefsNodes2, Waiting2} = request_while_waiting(RefsNodes, OkRefsNodes, Waiting, From, Ref, MyTime, OtherTime),
-	    wait(Nodes, Master, RefsNodes2, OkRefsNodes2, Waiting2, new_time(MyTime, OtherTime));
+	{request, From, Ref, OtherId, OtherTime} ->
+		NewTime = new_time(MyTime, OtherTime),
+	    {RefsNodes2, Waiting2} = request_while_waiting(RefsNodes, Waiting, From, Ref, MyId, NewTime, RequestTime, OtherId, OtherTime),
+	    wait(Nodes, Master, RefsNodes2, Waiting2, MyId, new_time(MyTime, OtherTime), RequestTime);
 	{ok, Ref, OtherTime} ->
-	    RefNode = lists:keyfind(Ref, 1, RefsNodes),
 	    RefsNodes2 = lists:keydelete(Ref, 1, RefsNodes),
-	    wait(Nodes, Master, RefsNodes2, [RefNode|OkRefsNodes], Waiting, new_time(MyTime, OtherTime));
+	    wait(Nodes, Master, RefsNodes2, Waiting, MyId, new_time(MyTime, OtherTime), RequestTime);
 	release ->
 	    ok(Waiting, MyTime),
-	    open(Nodes, MyTime)
+	    open(Nodes, MyId, MyTime)
     end.
 
-request_while_waiting(RefsNodes, OkRefsNodes, Waiting, From, Ref, MyTime, OtherTime) ->
+request_while_waiting(RefsNodes, Waiting, From, Ref, MyId, MyTime, RequestTime, OtherId, OtherTime) ->
+	MyTurn = my_priority_higher(MyId, RequestTime, OtherId, OtherTime),
     if
-	MyTime > OtherTime ->
-		NewTime = new_time(MyTime, OtherTime),
-	    From ! {ok, Ref, NewTime},
-	    OkRefsNodes2 = lists:keydelete(From, 2, OkRefsNodes),
-	    RefsNodes2 = lists:keydelete(From, 2, RefsNodes),
-	    R = make_ref(),
-	    From ! {request, self(), R, NewTime},
-	    Ret = {[{R, From}|RefsNodes2], OkRefsNodes2, Waiting};
-	true ->
-	    Ret = {RefsNodes, OkRefsNodes, [{From, Ref}|Waiting]}
+	MyTurn ->
+	    Ret = {RefsNodes, [{From, Ref}|Waiting]};
+    true ->
+        NewTime = new_time(MyTime, OtherTime),
+        From ! {ok, Ref, NewTime},
+        Ret = {RefsNodes, Waiting}
     end,
     Ret.
+
+my_priority_higher(MyId, RequestTime, OtherId, OtherTime) ->
+	if
+	RequestTime < OtherTime ->
+		Ret = true;
+	true ->
+		if
+		RequestTime == OtherTime ->
+			Ret = (MyId < OtherId);
+		true ->
+			Ret = false
+		end
+	end,
+	Ret.
+			
 
 new_time(MyTime) -> MyTime + 1.
 new_time(MyTime, OtherTime) ->
 	if
-		MyTime > OtherTime ->
-			Ret = new_time(MyTime);
-		true ->
-			Ret = new_time(OtherTime)
+	MyTime > OtherTime ->
+		Ret = new_time(MyTime);
+	true ->
+		Ret = new_time(OtherTime)
 	end,
 	Ret.
 
@@ -88,11 +100,11 @@ ok(Waiting, MyTime) ->
       end,
       Waiting).
 
-held(Nodes, Waiting, MyTime) ->
+held(Nodes, Waiting, MyId, MyTime) ->
     receive
-	{request, From, Ref, OtherTime} ->
-	    held(Nodes, [{From, Ref}|Waiting], new_time(MyTime, OtherTime));
+	{request, From, Ref, _, OtherTime} ->
+	    held(Nodes, [{From, Ref}|Waiting], MyId, new_time(MyTime, OtherTime));
 	release ->
 	    ok(Waiting, MyTime),
-	    open(Nodes, MyTime)
+	    open(Nodes, MyId, MyTime)
     end.
