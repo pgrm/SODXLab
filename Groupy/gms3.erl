251764f18caf1d0f69ffbf214ca36e8fec66f30a
Peter Grman <peter.grman@gmail.com>
2012-10-27 13:28:33 +0200

    gms 3 finished by the instructions
    It works more or less fine with 4 processes, even they sometimes get out
    of sync, they synchronize again, BUT - if running with 6 processes and
    the first disappears, the last one stops working, the second disappears
    and the 5th stops working. the other 2 still work

--- Groupy/gms3.erl
+++ Groupy/gms3.erl
@@ -9,7 +9,7 @@ start(Id) ->
 
 initMaster(Id, Rnd, Master) ->
     random:seed(Rnd, Rnd, Rnd),
-    leader(Id, Master, []).
+    leader(Id, Master, 1, []).
 
 start(Id, Grp) ->
     Self = self(),
@@ -19,10 +19,10 @@ initSlave(Id, Grp, Master) ->
     Self = self(),
     Grp ! {join, Self},
     receive
-        {view, State, Leader, Peers} ->
+        {view, N, State, Leader, Peers} ->
             erlang:monitor(process, Leader),
             Master ! {ok, State},
-            slave(Id, Master, Leader, Peers)
+            slave(Id, Master, Leader, N, State, Peers)
         after 1000 ->
             Master ! {error, "no reply from leader"}
     end.
@@ -31,53 +31,56 @@ slave(Id, Master, Leader, N, Last, Peers) ->
     receive
         {mcast, Msg} ->
             Leader ! {mcast, Msg},
-            slave(Id, Master, Leader, Peers);
+            slave(Id, Master, Leader, N, Last, Peers);
         {join, Peer} ->
             Leader ! {join, Peer},
-            slave(Id, Master, Leader, Peers);
-        {msg, Msg} ->
+            slave(Id, Master, Leader, N, Last, Peers);
+        {msg, N, Msg} ->
             Master ! {deliver, Msg},
-            slave(Id, Master, Leader, Peers);
-        {view, _, _, View} ->
-            slave(Id, Master, Leader, View);
+            slave(Id, Master, Leader, N + 1, Msg, Peers);
+        {msg, I, _} when I < N ->
+            slave(Id, Master, Leader, N, Last, Peers);    
+        {view, _, _, _, View} ->
+            slave(Id, Master, Leader, N, Last, View);
         {'DOWN', _Ref, process, Leader, _Reason} ->
-            election(Id, Master, Peers);
+            election(Id, Master, N, Last, Peers);
         stop ->
             ok;
         Error ->
             io:format("gms ~w: slave, strange message ~w~n", [Id, Error])
     end.
 
-election(Id, Master, [Leader|Rest]) ->
+election(Id, Master, N, Last, [Leader|Rest]) ->
     if
         Leader == self() ->
-            leader(Id, Master, Rest);
+            bcast(Id, {msg, N, Last}, Rest),
+            leader(Id, Master, N, Rest);
         true ->
             erlang:monitor(process, Leader),
-            slave(Id, Master, Leader, Rest)
+            slave(Id, Master, Leader, N, Last, Rest)
     end.
 
-leader(Id, Master, Peers) ->
+leader(Id, Master, N, Peers) ->
     receive
         {mcast, Msg} ->
-            bcast(Id, {msg, Msg}, Peers),
+            bcast(Id, {msg, N, Msg}, Peers),
             Master ! {deliver, Msg},
-            leader(Id, Master, Peers);
+            leader(Id, Master, N + 1, Peers);
         {join, Peer} ->
             Master ! request,
-            joining(Id, Master, Peer, Peers);
+            joining(Id, Master, N, Peer, Peers);
         stop ->
             ok;
         Error ->
             io:format("gms ~w: leader, strange message ~w~n", [Id, Error])
     end.
 
-joining(Id, Master, Peer, Peers) ->
+joining(Id, Master, N, Peer, Peers) ->
     receive
         {ok, State} ->
             Peers2 = lists:append(Peers, [Peer]),
-            bcast(Id, {view, State, self(), Peers2}, Peers2),
-            leader(Id, Master, Peers2);
+            bcast(Id, {view, N, State, self(), Peers2}, Peers2),
+            leader(Id, Master, N, Peers2);
         stop ->
             ok
     end.
